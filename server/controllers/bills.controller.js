141e99cad8ee640361b0b19de9a25e7a20dbe5e2
MadhuMallipudi <61326706+MadhuMallipudi@users.noreply.github.com>
2020-10-17 13:19:22 +0530

    removed unwanted code

--- server/controllers/bills.controller.js
+++ server/controllers/bills.controller.js
@@ -23,11 +23,26 @@ const bills_controller =  () => {
    };
    const _getCounts = async  (req,res) => {
         try{
-            let result =  await  bills.find();
-            let dayCount = result.filter(item => {  return moment(item.created_date).format("DD-MM-yyyy") == moment().format("DD-MM-yyyy")}).length;
-            let monthCount = result.filter(item => { return moment(item.created_date).getMonth == moment().getMonth}).length;
-            let yearCount = result.filter(item => { return moment(item.created_date).getFullYear == moment().getFullYear}).length;
-            res.status(200).send({status:200,message:"fetched successfully",dayCount:dayCount,monthCount:monthCount,yearCount:yearCount});  
+            let result =  await  bills.find().populate("item_id").exec();
+            let dayCount = result.filter(item => {  return moment(item.created_date).format("DD-MM-yyyy") == moment().format("DD-MM-yyyy")});
+            let monthCount = result.filter(item => { return moment(item.created_date).getMonth == moment().getMonth});
+            let yearCount = result.filter(item => { return moment(item.created_date).getFullYear == moment().getFullYear});
+            console.log("daycount",monthCount);
+            // let dailyCount = await getDailySales(dayCount);
+            
+            let dailySales = 0;
+            let montlySales =0;
+            let yearlySales =0;
+            (dayCount || []).forEach((item)=>{
+                return dailySales += item.quantity * item.item_id.price;
+            });
+            (monthCount || []).forEach((item)=>{
+                return montlySales += item.quantity * item.item_id.price;
+            });
+            (yearCount || []).forEach((item)=>{
+                return yearlySales += item.quantity * item.item_id.price;
+            });              
+            res.status(200).send({status:200,message:"fetched successfully",dayCount:dailySales,monthCount:montlySales,yearCount:yearlySales});  
         } catch(ex){
             res.status(500).send({status:500,message:ex.message});
         }
